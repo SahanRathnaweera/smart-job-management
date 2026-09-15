caa712d0f54d721a49d470fa93988eff9f7c6512
SahanRathnaweera <sahantharuka0909@gmail.com>
2026-09-15 13:58:22 +0530

    Added PostgreSQL to databases

--- backend/src/main/java/com/system/jobmanagement/entity/Application.java
+++ backend/src/main/java/com/system/jobmanagement/entity/Application.java
@@ -1,4 +1,40 @@
 package com.system.jobmanagement.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "applications")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Application {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "student_id", nullable = false)
+    private User student;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "job_id", nullable = false)
+    private Job job;
+
+    @Enumerated(EnumType.STRING)
+    private ApplicationStatus status = ApplicationStatus.PENDING;
+
+    private String resumeUrl;
+    private LocalDateTime appliedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.appliedAt = LocalDateTime.now();
+    }
 }
--- backend/src/main/java/com/system/jobmanagement/entity/Job.java
+++ backend/src/main/java/com/system/jobmanagement/entity/Job.java
@@ -1,5 +1,44 @@
 package com.system.jobmanagement.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "jobs")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 
 public class Job {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String description;
+
+    private String companyName;
+    private String location;
+    private String jobType;
+    private Double salary;
+
+    @ManyToOne(fetch = FetchType.LAZY) //data loading controll
+    @JoinColumn(name = "posted_by", nullable = false)
+    private User company;
+
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
 }
